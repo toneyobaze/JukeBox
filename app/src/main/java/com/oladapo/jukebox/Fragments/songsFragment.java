04e4f86daf6bbbe67e715e3f70194d3f14f9705d
oladapo <toneyobaze@gmail.com>
2019-01-20 17:41:31 +0000

    Added code code to get songs

--- app/src/main/java/com/oladapo/jukebox/Fragments/songsFragment.java
+++ app/src/main/java/com/oladapo/jukebox/Fragments/songsFragment.java
@@ -1,6 +1,242 @@
 package com.oladapo.jukebox.Fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
 
-public class songsFragment extends Fragment {
+import com.oladapo.jukebox.MusicController;
+import com.oladapo.jukebox.MusicService;
+import com.oladapo.jukebox.R;
+import com.oladapo.jukebox.Song;
+import com.oladapo.jukebox.SongAdapter;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static android.provider.MediaStore.MediaColumns.TITLE;
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
+
+public class songsFragment extends Fragment implements MediaController.MediaPlayerControl {
+
+    private ArrayList<Song> songList;
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound = false;
+
+    RelativeLayout trackRelLay;
+
+
+    public songsFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.songs_fragment, container, false);
+        trackRelLay = v.findViewById(R.id.trackRelLay);
+
+        return v;
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Objects.requireNonNull(getActivity()),Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+                return;
+            }
+        }
+
+        ListView songView = view.findViewById(R.id.song_list);
+        songList = new ArrayList<>();
+
+        getSongList();
+
+        setController(view);
+
+        SongAdapter songAdt = new SongAdapter(getActivity(), songList);
+        songView.setAdapter(songAdt);
+
+        songView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                songPicked(view);
+            }
+        });
+
+    }
+
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+    public void getSongList() {
+
+        ContentResolver musicResolver = Objects.requireNonNull(getActivity()).getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        @SuppressLint("Recycle") Cursor musicCursor = musicResolver.query(musicUri, null, null, null, TITLE);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(getContext(), MusicService.class);
+            Objects.requireNonNull(getActivity()).bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            getActivity().startService(playIntent);
+        }
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+
+    private void setController(View view){
+
+        MusicController controller = new MusicController(getActivity());
+
+        controller.setMediaPlayer(this);
+        controller.setAnchorView(view.findViewById(R.id.song_list));
+        controller.setEnabled(true);
+
+    }
+
+    /*private void playNext(){
+        musicSrv.playNext();
+        controller.show(0);
+    }
+
+    //play previous
+    private void playPrev(){
+        musicSrv.playPrev();
+        controller.show(0);
+    }*/
+
+    private void songPicked(View view) {
+
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+        if (musicBound) musicSrv.unbindService(musicConnection);
+        Objects.requireNonNull(getActivity()).stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
 }
---
+++ app/src/main/java/com/oladapo/jukebox/MusicService.java
@@ -0,0 +1,161 @@
+package com.oladapo.jukebox;
+
+import android.app.Service;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+
+    //media player
+    private MediaPlayer player;
+    //song list
+    private ArrayList<Song> songs;
+    //current position
+    private int songPosn;
+
+    private final IBinder musicBind = new MusicBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+        if(player.getCurrentPosition()>0){
+            mp.reset();
+            playNext();
+        }
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+
+        mp.reset();
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+        mp.start();
+
+    }
+
+    public void onCreate(){
+
+        super.onCreate();
+        songPosn=0;
+        player = new MediaPlayer();
+
+        initMusicPlayer();
+
+    }
+
+    public void initMusicPlayer(){
+
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs=theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        public MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    public void playNext(){
+        songPosn++;
+        if(songPosn >= songs.size()) songPosn=0;
+        playSong();
+    }
+
+    public void playPrev(){
+        songPosn--;
+        if(songPosn <0) songPosn=songs.size()-1;
+        playSong();
+    }
+
+    public void playSong(){
+
+        player.reset();
+
+        //get song
+        Song playSong = songs.get(songPosn);
+//get id
+        long currSong = playSong.getID();
+//set uri
+        Uri trackUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                currSong);
+
+        try{
+            player.setDataSource(getApplicationContext(), trackUri);
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+
+        player.prepareAsync();
+
+    }
+
+    public void setSong(int songIndex){
+        songPosn=songIndex;
+    }
+
+    public int getPosn(){
+        return player.getCurrentPosition();
+    }
+
+    public int getDur(){
+        return player.getDuration();
+    }
+
+    public boolean isPng(){
+        return player.isPlaying();
+    }
+
+    public void pausePlayer(){
+        player.pause();
+    }
+
+    public void seek(int posn){
+        player.seekTo(posn);
+    }
+
+    public void go(){
+        player.start();
+    }
+
+}
